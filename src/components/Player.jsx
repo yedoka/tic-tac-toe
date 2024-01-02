2cdd3fbb7c9ea2a501ecba14a9c768a27a5129b3
yedoka <akedilbekbolatov@gmail.com>
2024-01-02 13:45:40 +0100

    edit button

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,33 @@
-export default function Player({ name, symbol }) {
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editiblePlayerName = <span className="player-name">{playerName}</span>;
+  // let btnCaption = "Edit";
+
+  if (isEditing) {
+    editiblePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+    // btnCaption = "Save"
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {editiblePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
